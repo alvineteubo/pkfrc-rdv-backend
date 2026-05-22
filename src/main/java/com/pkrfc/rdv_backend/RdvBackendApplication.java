3b84d797f037676ae9f5d7613bc23c5a761c9bee
alvineteubo <alvineteubo@gmail.com>
2026-05-22 22:58:05 +0100

    initialisation du projet spring boot

---
+++ src/main/java/com/pkrfc/rdv_backend/RdvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pkrfc.rdv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkrfc/rdv_backend/RdvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkrfc.rdv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
